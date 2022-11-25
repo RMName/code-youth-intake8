02ba4474961a290e326df1428ed9818fa01224e5
RMName <117660812+RMName@users.noreply.github.com>
2022-11-25 09:05:56 -0500

    learning js

---
+++ JS/print.js
@@ -0,0 +1,41 @@
+console.log("Welcome to Code Youth");
+
+///"quotiation marks means it'll display it as a string"
+let a = 5;
+let b = 3;
+
+let c = a * b
+console.log("sum of",a, "and",b,"is:",c);
+const pi=3.142;
+console.log("pi:", pi);
+
+
+///string
+
+let name1 = "John";
+console.log(typeof pi);
+let name2 = "Ray"
+let check = true;
+let check1= (a==10)
+
+///boolean
+
+//console.log(typeof check1);
+
+///object
+///let student = {"name":"john"};
+//console.log("student:", student);
+//console.log("type of student:",)
+
+
+//let a = 5
+//let b = 13
+//let c = a * b
+//console.log("Area of"a, "and",b,"is:",c);
+
+const arr = [1, 2, 3, 4, 5];
+const average = arr.reduce((a, b) => a + b, 0) / arr.length;
+
+console.log(average);
+
+
